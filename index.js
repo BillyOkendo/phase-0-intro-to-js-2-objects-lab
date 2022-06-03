1cf948710037e88c87bc080116590182ef17c252
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-06-03 19:10:54 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = {
+    name: "Karwirwa",
+    streetAddress: "02000"
+
+}
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const employeeTwo = {...employee };
+    employeeTwo[key] = value;
+    return employeeTwo;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const employeeTwo = {...employee };
+    delete employeeTwo[key];
+    return employeeTwo;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
